1e4d53583c12d08158f252697e6c4e7b327f18df
Aleksey Petkun <aleksey_2710@mail.ru>
2023-08-10 22:06:15 +0300

    initial commit

---
+++ newsFeedApp/src/main/java/com/example/newsFeedApp/NewsFeedAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsFeedApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsFeedAppApplication.class, args);
+	}
+
+}

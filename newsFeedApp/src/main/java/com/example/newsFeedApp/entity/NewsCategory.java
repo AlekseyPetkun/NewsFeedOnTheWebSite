61e66b59808e98789ea2c687b9026e6a23e11247
Aleksey Petkun <aleksey_2710@mail.ru>
2023-08-12 13:39:50 +0300

    create enum NewsCategory

--- newsFeedApp/src/main/java/com/example/newsFeedApp/entity/NewsCategory.java
+++ newsFeedApp/src/main/java/com/example/newsFeedApp/entity/NewsCategory.java
@@ -1,15 +1,31 @@
 package com.example.newsFeedApp.entity;
 
+import com.example.newsFeedApp.repository.CategoryRepository;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.List;
 
 /**
  * Категория новости
  */
 public enum NewsCategory {
 
-    POLITICAL,
-    ECONOMIC,
-    SPORTS,
-    SOCIAL,
-    SCIENTIFIC
+    POLITICAL("Политика"),
+    ECONOMIC("Экономика"),
+    SPORTS("Спорт"),
+    SOCIAL("Социальная"),
+    SCIENTIFIC("Научная");
+
+    private final String translation;
+
+    NewsCategory(String translation) {
+        this.translation = translation;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
 }

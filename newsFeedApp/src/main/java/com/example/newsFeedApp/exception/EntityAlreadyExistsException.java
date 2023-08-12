2a3bffcf280eab87285cc34b960c871d367be12b
Aleksey Petkun <aleksey_2710@mail.ru>
2023-08-13 01:04:15 +0300

    create Exceptions

---
+++ newsFeedApp/src/main/java/com/example/newsFeedApp/exception/EntityAlreadyExistsException.java
@@ -0,0 +1,17 @@
+package com.example.newsFeedApp.exception;
+
+/**
+ * Ошибка проверки наличия сущности в БД
+ */
+public class EntityAlreadyExistsException extends RuntimeException{
+
+    private final String text;
+    public EntityAlreadyExistsException(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Сущность: " + text + " уже существует!";
+    }
+}
---
+++ newsFeedApp/src/main/java/com/example/newsFeedApp/exception/NotFindNewsCategoryException.java
@@ -0,0 +1,17 @@
+package com.example.newsFeedApp.exception;
+
+/**
+ * Ошибка поиска категории
+ */
+public class NotFindNewsCategoryException extends RuntimeException{
+
+    private final String text;
+    public NotFindNewsCategoryException(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Категория: " + text + " не существует!";
+    }
+}

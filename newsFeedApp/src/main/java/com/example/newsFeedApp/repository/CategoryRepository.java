8ff8e04eadc5eb8b7b7fcb122f3b125f8d80cda8
Aleksey Petkun <aleksey_2710@mail.ru>
2023-08-15 00:39:03 +0300

    create CategoryRepository

--- newsFeedApp/src/main/java/com/example/newsFeedApp/repository/CategoryRepository.java
+++ newsFeedApp/src/main/java/com/example/newsFeedApp/repository/CategoryRepository.java
@@ -4,8 +4,16 @@ import com.example.newsFeedApp.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Репозиторий для категорий
+ */
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    /**
+     * Поиск по названию категории
+     * @param newsCategory категория
+     * @return сущность
+     */
     Category findByNewsCategoryContainsIgnoreCase(String newsCategory);
 }

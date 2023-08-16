7900135af1380e693b30d917dd2face924572430
Aleksey Petkun <aleksey_2710@mail.ru>
2023-08-16 22:30:21 +0300

    creating pagination in the repository

--- newsFeedApp/src/main/java/com/example/newsFeedApp/repository/FeedRepository.java
+++ newsFeedApp/src/main/java/com/example/newsFeedApp/repository/FeedRepository.java
@@ -1,6 +1,8 @@
 package com.example.newsFeedApp.repository;
 
 import com.example.newsFeedApp.entity.Feed;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,23 +18,35 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
      * Поиск новостей по категории
      *
      * @param newsCategory категория
+     * @param pageable     пагинация
      * @return список найденный новостей
      */
-    List<Feed> findByCategoryId(Long newsCategory);
+    List<Feed> findByCategoryId(Long newsCategory, Pageable pageable);
 
     /**
      * Поиск новостей по названию
      *
-     * @param title название
+     * @param title    название
+     * @param pageable пагинация
      * @return список найденных новостей
      */
-    List<Feed> findByTitleContainingIgnoreCase(String title);
+    List<Feed> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
     /**
      * Поиск новостей по содержанию
      *
-     * @param content содержание
+     * @param content  содержание
+     * @param pageable пагинация
      * @return список найденных новостей
      */
-    List<Feed> findByContentContainingIgnoreCase(String content);
+    List<Feed> findByContentContainingIgnoreCase(String content, Pageable pageable);
+
+    /**
+     * Показать все сохраненные новости
+     *
+     * @param pageable пагинация
+     * @return страница новостей
+     */
+    @Override
+    Page<Feed> findAll(Pageable pageable);
 }

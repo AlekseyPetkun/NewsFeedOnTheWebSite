a11ace7e547b84f52534281e2fe25fadcfdc456a
Aleksey Petkun <aleksey_2710@mail.ru>
2023-08-17 11:57:51 +0300

    creating dto ResponseWrapper

---
+++ newsFeedApp/src/main/java/com/example/newsFeedApp/dto/ResponseWrapperCategories.java
@@ -0,0 +1,19 @@
+package com.example.newsFeedApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * DTO всех категорий.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseWrapperCategories {
+
+    private Integer count; // общее количество категорий
+    private List<CategoryDto> results; // все категории
+}

c8f7aee7879f3eebd3531a564e9c5fc02e647301
Aleksey Petkun <aleksey_2710@mail.ru>
2023-09-02 01:35:58 +0300

    creating openapi.yaml

--- newsFeedApp/src/main/java/com/example/newsFeedApp/NewsFeedAppApplication.java
+++ newsFeedApp/src/main/java/com/example/newsFeedApp/NewsFeedAppApplication.java
@@ -1,9 +1,15 @@
 package com.example.newsFeedApp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "NewsFeedApp",
+		description = "Веб-приложение по управлению новостной лентой на сайте"
+))
 public class NewsFeedAppApplication {
 
 	public static void main(String[] args) {
